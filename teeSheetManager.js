0e7919ee834e3796a0fa8b3a143a508a311485cc
Brock Bjerke <159338581+brockBjerke@users.noreply.github.com>
2024-03-14 12:09:31 -0500

    working on fillint tee sheet

---
+++ teeSheetManager.js
@@ -0,0 +1,18 @@
+
+function createTeeSheet() {
+    
+    
+
+
+}
+
+function createTimeinterval(){
+    let times = [];
+    for (let hour = 0; hour < 24; hour++) {
+        for (let minute = 0; minute < 60; minute += 5) {
+            let time = `${hour.toString().padStart(2, '0')}:${minute.toString().padStart(2, '0')}`;
+            times.push(time);
+        }
+    }
+    return time;
+}
