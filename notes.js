bf364a898ba142259aab6e8e0481173f027ac4c4
torrey-vicchiollo <159338797+torrey-vicchiollo@users.noreply.github.com>
2024-03-24 14:39:54 -0500

    i brokey

--- notes.js
+++ notes.js
@@ -1,6 +1,6 @@
 function addNotes(){
     let currentNotes = document.getElementById("notesBody").innerHTML;
     let newNotes = document.getElementById("notesTextArea").value;
-    document.getElementById("notesBody").innerHTML = currentNotes + <br></br> + "→ " + newNotes + <br></br>;
+    document.getElementById("notesBody").innerHTML = currentNotes + "<br />" + "→ " + newNotes + "<br />";
     document.getElementById("notesTextArea").value = ""
 }
