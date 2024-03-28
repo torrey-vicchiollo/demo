a36513602393ce161000e676b4ca2ba08eea3696
torrey-vicchiollo <torreyvicchiollo@outlook.com>
2024-03-27 22:10:45 -0500

    php2

--- includes/formhandler.inc.php
+++ includes/formhandler.inc.php
@@ -1,7 +1,7 @@
 <?php
     if($_SERVER["REQUEST_METHOD"] == "POST"){
         $date = date("Y/m/d");
-        $body = $_POST["notesTextArea"];
+        $body = htmlspecialchars($_POST["notesTextArea"]);
         try{
             require_once "dbh.inc.php";
             $query = "INSERT INTO notes (date, body) VALUES (?, ?);";
@@ -9,6 +9,7 @@
             $stmt->execute([$date, $body]);
             $pdo = null;
             $stmt = null;
+            header("Location: ../index.php");
             die();
         }catch(PDOException $e){
             die("Query failed: " . $e->getMessage());
