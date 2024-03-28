31ccf5e66d8c18b7da09be6bc1989e822115669f
torrey-vicchiollo <torreyvicchiollo@outlook.com>
2024-03-28 12:49:05 -0500

    a

--- includes/notesSubmit.inc.php
+++ includes/notesSubmit.inc.php
@@ -2,6 +2,6 @@
     include_once 'dbh.inc.php';
     $date = date("Y/m/d");
     $body = $_POST['notesTextArea'];
-    $sql = "REPLACE INTO notes (date, body) VALUES ($date, $body);";
+    $sql = "REPLACE INTO notes (date, body) VALUES ('$date', '$body');";
     mysqli_query($conn, $sql);
     header("Location: ../index.php");
