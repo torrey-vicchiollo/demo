f63fae4f56ae28b91c770656bf35fc5c70130369
torrey-vicchiollo <torreyvicchiollo@outlook.com>
2024-04-02 21:25:08 -0500

    im dumb

--- includes/teeSheetSubmit.inc.php
+++ includes/teeSheetSubmit.inc.php
@@ -4,14 +4,14 @@
     $endOfDay = $_POST['teeTimeSelectEnd'];                                         //end of day
     $interval = $_POST['intervalSelector'];                                         //interval
     $year = $_POST['yearForTeeSheet'];                                              //year
-    $month = $_POST['monthForTeeSheet'];                                            //month
-    $day = $_POST['dayForTeeSheet'] + 1;                                             //day
+    $month = $_POST['monthForTeeSheet'] + 1;                                        //month
+    $day = $_POST['dayForTeeSheet'];                                                //day
     $date = $month . "/" . $day . "/" . $year;                                      //date
     
     $sql = "SELECT * FROM teesheets WHERE date = '$date';";                          //first call which checks if teesheet for day already exists  
     $result = mysqli_query($conn, $sql);                                            //sets result variable
     $resultCheck = mysqli_num_rows($result);                                        //sets result check variable
-    if($resultCheck < 1){                                                           //if there isn't already a teesheet for the date
+    if(($resultCheck < 1) && ($startOfDay != $endOfDay)){                           //if there isn't already a teesheet for the date and sod and eod do not equal
         $sql = "INSERT INTO teesheets (date, startOfDay, endOfDay, intrvl)
         VALUES ('$date', '$startOfDay', '$endOfDay', '$interval');";                //query for new teesheet
         mysqli_query($conn, $sql);                                                  //query to database
